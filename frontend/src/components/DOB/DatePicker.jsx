32ac94fee9ef03f23649b2f6f862ee3608194e6d
karanagarwal12 <karanagarwal200505@gmail.com>
2024-05-01 00:55:53 +0530

    some changes

--- frontend/src/components/DOB/DatePicker.jsx
+++ frontend/src/components/DOB/DatePicker.jsx
@@ -3,12 +3,20 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers';
+import dayjs from 'dayjs';
 
 export default function BasicDatePicker({label, handler, id, name}) {
+  const today = dayjs();
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DemoContainer components={['DatePicker']}>
-        <DatePicker label={label} onChange={(value) => {handler(name, value)}} id={id} name={name} />
+        <DatePicker
+          label={label}
+          onChange={(value) => {handler(name, value)}}
+          id={id}
+          name={name}
+          maxDate={today}
+        />
       </DemoContainer>
     </LocalizationProvider>
   );
