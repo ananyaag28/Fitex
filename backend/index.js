3a0052845116490842cd033767ee30b7057de1b4
monarxxjain <jainmonark1000@gmail.com>
2024-04-30 06:36:32 +0530

    Prisma Init

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const cookieParser = require("cookie-parser");
 const cors = require('cors')
-// const prisma = require('./db');
-// const bcrypt = require('bcrypt');
 const authRouter = require('./routes/authRoutes');
 const dealRouter = require('./routes/dealRoutes')
 const sellerRouter = require('./routes/sellerRoutes')
@@ -15,28 +13,11 @@ app.use(cookieParser());
 // cors policy
 app.use(
     cors({
-      origin: ['http://localhost:3000', 'https://invoicing-web.vercel.app'], // Your allowed origin
+      origin: ['http://localhost:3000'], // Your allowed origin
       credentials: true, // Allow cookies to be sent
     })
 );
 
-
-
-async function main() {
-
-  const hashedPassword = await bcrypt.hash("7410", 12)
-  const employee = await prisma.employee.create({
-    data: {
-      name: "Monark Jain",
-      email: "lcs2022033@gmail.com",
-      role: "ADMIN",
-      password: hashedPassword
-    }
-  })
-  console.log("Default Admin Added")
-}
-
-// main()
 
 app.use('/auth', authRouter)
 app.use('/deal', dealRouter)
